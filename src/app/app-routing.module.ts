23ce3f5741e6be895b5469ae98cfe31d2d47389e
fabricoliveira <oliveira.fabricio.santos@gmail.com>
2022-01-06 22:47:07 -0300

    Criando o Read do Crud de Categoria com mat-table

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { HomeComponent } from './components/views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{
-  path: '',
-  component: HomeComponent
-}];
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
+import { HomeComponent } from './components/views/home/home.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent,
+  },
+  {
+    path: "categorias",
+    component: CategoriaReadComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './components/template/footer/footer.component';
 import { HeaderComponent } from './components/template/header/header.component';
 import { NavComponent } from './components/template/nav/nav.component';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/views/home/home.component';
 
 @NgModule({
@@ -21,6 +23,7 @@ import { HomeComponent } from './components/views/home/home.component';
     FooterComponent,
     NavComponent,
     HomeComponent,
+    CategoriaReadComponent,
   ],
   imports: [
     BrowserModule,
@@ -31,6 +34,7 @@ import { HomeComponent } from './components/views/home/home.component';
     MatListModule,
     MatIconModule,
     MatCardModule,
+    MatTableModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
